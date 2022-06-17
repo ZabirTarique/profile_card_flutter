1506f5b055b13276b0ffb606edfff0b62e3dbeee
KMZabirTarique <k.m.zabirtarique.baust@gmail.com>
2022-06-18 00:48:13 +0600

    Add profile card project

---
+++ profile_card/lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(child: myCard(context)),
+      ),
+    );
+  }
+
+  Widget myCard(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const CircleAvatar(
+          radius: 50.0,
+          backgroundImage: AssetImage("assets/images/zabir2.png"),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          "K.M. Zabir Tarique",
+          style: TextStyle(
+              fontFamily: 'Pacifico', color: Colors.white, fontSize: 20),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        const Text(
+          "k.m.zabir.tarique.baust@gmail.com",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          "FLUTTER DEVELOPER",
+          style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+              color: Colors.teal.shade100,
+              fontWeight: FontWeight.bold,
+              fontSize: 20),
+        ),
+        const SizedBox(
+          width: 250.0,
+          child: Divider(
+            height: 20,
+            thickness: 1.0,
+            color: Colors.white,
+          ),
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),
+              side: const BorderSide(color: Colors.red)),
+          color: Colors.white,
+          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: const ListTile(
+            leading: Icon(
+              Icons.phone,
+              color: Colors.teal,
+            ),
+            title: Text(
+              "+88 01762786937",
+              style: TextStyle(
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
+            ),
+          ),
+        ),
+
+
+        Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: const BorderSide(color: Colors.red)
+            ),
+            color: Colors.white,
+            margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: Image.asset('assets/images/facebook.png',
+                          fit: BoxFit.cover)),
+                  const SizedBox(width: 20,),
+
+                  SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: Image.asset('assets/images/github.png',
+                          fit: BoxFit.cover)),
+                  const SizedBox(width: 20,),
+                  SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: Image.asset('assets/images/gmail.png',
+                          fit: BoxFit.cover)),
+                  const SizedBox(width: 20,),
+                  SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: Image.asset('assets/images/linkedin.png',
+                          fit: BoxFit.cover)),
+                  // SizedBox(
+                  //     height: 30,
+                  //     width: 100,
+                  //     child: Image.asset('assets/images/youtube.png',
+                  //         fit: BoxFit.cover)),
+                ],
+              ),
+            )
+        )
+      ],
+    );
+  }
+
+/*
+* Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.mail,
+                  color: Colors.teal,
+                ),
+                SizedBox(width: 10.0),
+                Text("fahim@gmail.com",style: TextStyle(
+                    color: Colors.teal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),)
+
+              ],
+            )
+*
+* */
+  Widget layoutChallenge43(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: double.infinity,
+          width: 100.0,
+          color: Colors.red,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.green,
+            ),
+          ],
+        ),
+        Container(
+          height: double.infinity,
+          width: 100.0,
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+}
